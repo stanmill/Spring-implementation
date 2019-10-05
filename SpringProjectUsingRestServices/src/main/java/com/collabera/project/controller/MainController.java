953ab2d6300aaffb55ff500128814b644dcd21af
stanmill <stanley.pena29@gmail.com>
2019-10-04 23:27:12 -0400

    Update MainController.java

--- SpringProjectUsingRestServices/src/main/java/com/collabera/project/controller/MainController.java
+++ SpringProjectUsingRestServices/src/main/java/com/collabera/project/controller/MainController.java
@@ -1,5 +1,16 @@
 package com.collabera.project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.collabera.project.model.CarModel;
+
+@Controller
 public class MainController {
+	
+	@RequestMapping(value="/login")
+	private String indexPage(CarModel car) {
+		
+	}
 
 }
